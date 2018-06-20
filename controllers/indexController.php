ebc93e9058f33a020c4a6a14b185bd21947e782b
Lautaro <lautarosilva9500@gmail.com>
2018-06-20 14:15:30 -0300

    armando postulacion, cancelar postulacion y cancelar viaje

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -8,9 +8,6 @@ class indexController extends Controller
 
     public function index()
     {
-//        require_once ROOT . 'controllers' . DS . 'notificacionController.php';
-//        $notificaciones = new notificacionController();
-//        $notificaciones->crearNotificacion("el usuario ".Session::get("id_usuario")." cancelo el viaje al que te habias postulado.", array(10, 11, 12, 13, 14));
         $this->_view->renderizar('index', 'index');
     }
 }

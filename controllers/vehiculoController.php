367ea85d550147dd8aaf60d4ec0863d3a22c6a97
Lautaro <lautarosilva9500@gmail.com>
2018-06-20 16:27:55 -0300

    seleccion de asientos

--- controllers/vehiculoController.php
+++ controllers/vehiculoController.php
@@ -3,6 +3,7 @@
 class vehiculoController extends Controller {
 
     private $_registro;
+    private $_vehiculo;
     private $_usuario;
 
     public function __construct() {
@@ -10,6 +11,8 @@ class vehiculoController extends Controller {
         require_once ROOT . 'models' . DS . 'registroModel.php';
         require_once ROOT . 'models' . DS . 'usuarioModel.php';
         require_once ROOT . 'models' . DS . 'vehiculoModel.php';
+        require_once ROOT . 'models' . DS . 'vehiculoModel.php';
+        $this->_vehiculo = new vehiculoModel();
         $this->_usuario = new usuarioModel();
         $this->_registro = new vehiculoModel();
     }
@@ -31,41 +34,44 @@ class vehiculoController extends Controller {
     public function validarPatente($form) {
         require_once ROOT . 'models' . DS . 'vehiculoModel.php';
         $vehiculoModel = new vehiculoModel();
-        $err = $vehiculoModel->consultarPatente($form); 
+        $err = $vehiculoModel->consultarPatente($form);
         if ($err['cantidad'] > 0) {
             return false;
         } else {
             return true;
         }
     }
-    public function eliminarVehiculo($id){
-                 require_once ROOT . 'models' . DS . 'vehiculoModel.php';
-                $vehiculoModel = new vehiculoModel();
-                $vehiculoModel->darDeBaja($id);
-                Session::setMessage("Vehiculo dado de baja", SessionMessageType::Success);
-                $this->redireccionar("perfil");
+
+    public function eliminarVehiculo($id) {
+        require_once ROOT . 'models' . DS . 'vehiculoModel.php';
+        $vehiculoModel = new vehiculoModel();
+        $vehiculoModel->darDeBaja($id);
+        Session::setMessage("Vehiculo dado de baja", SessionMessageType::Success);
+        $this->redireccionar("perfil");
     }
+
     public function crear() {
         if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
             Session::setMessage("Intento de acceso incorrecto a la funcion.", SessionMessageType::Error);
             $this->redireccionar("registro");
-           
         }
 
         $form = array();
         $form['patente'] = $this->getAlphaNum('patente');
-            if ($this->validarPatente($form)) {
+        if ($this->validarPatente($form)) {
             $form['marca'] = $this->getAlphaNum('marca');
             $form['modelo'] = $this->getAlphaNum('modelo');
 
             $form['asientos'] = $this->getAlphaNum('asientos');
             $form['baul'] = $this->getAlphaNum('baul');
             $form["baul"] = $form["baul"] == "on" ? 1 : 0;
-             Session::set("form", $form);
+            Session::set("form", $form);
             $errors = $this->validarAltaVehiculo();
+        } else
+            $errors = 1;
+        if ($errors == 1) {
+            Session::setFormErrors("patente", "Esa patente Ud. la tiene cargada.");
         }
-        else $errors=1;
-        if ($errors==1) {Session::setFormErrors("patente", "Esa patente Ud. la tiene cargada.");}
         if (!$errors) {
             try {
                 require_once ROOT . 'models' . DS . 'vehiculoModel.php';
@@ -98,6 +104,20 @@ class vehiculoController extends Controller {
             $this->redireccionar();
         }
         $this->_view->renderizar('lista', 'vehiculo');
+    }
+
+    public function getVehiculoJson() {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            Session::setMessage("Intento de acceso incorrecto a la funcion.", SessionMessageType::Error);
+            $this->redireccionar("registro");
+        }
+
+        if (!Session::get('autenticado')) {
+            $this->redireccionar();
+        }
+        $idVehiculo = $this->getPostParam("idVehiculo");
+        $vehiculo = $this->_vehiculo->getVehiculo($idVehiculo);
+        echo json_encode(array("vehiculo" => $vehiculo));
     }
 
     public function validarAltaVehiculo() {

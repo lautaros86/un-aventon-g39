250cbb1af5a2dcca66a5b15bc512ebefca7297b6
Lautaro <lautarosilva9500@gmail.com>
2018-06-27 20:15:46 -0300

    extraer dinero

--- controllers/walletController.php
+++ controllers/walletController.php
@@ -14,6 +14,17 @@ class walletController extends Controller {
         
     }
 
+    public function cobrar($monto) {
+        $saldo = $this->_wallet->getSaldo(Session::get("id_usuario"));
+        if ($monto > $saldo) {
+            Session::setMessage("Ud no cuenta con el saldo ingresado.", SessionMessageType::Error);
+        }else{
+            $this->_wallet->extraer(Session::get("id_usuario"), $monto);
+            Session::setMessage("Se tranfirieron $" . $monto . " a su cuenta bancaria.", SessionMessageType::Success);
+        }
+        $this->redireccionar("perfil#wallet");
+    }
+
 }
 
 ?>

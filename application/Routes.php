d1cc75a6f97b4e779383b03564f4a6c05978c928
Lautaro <lautarosilva9500@gmail.com>
2018-06-28 22:58:27 -0300

    fix preguntas frecuentes

--- application/Routes.php
+++ application/Routes.php
@@ -4,6 +4,7 @@ $routes = array();
 $routes['registro'] = 'usuario/registro';
 $routes['perfil'] = 'usuario/verUsuario';
 $routes['cerrarsesion'] = '/login/cerrar';
+$routes['preguntas-frecuentes'] = '/paginaEstatica/preguntasFrecuentes';
 $routes['pruebas/(:any)/(:any)'] = 'usuario/test/$1/$2';
 
 

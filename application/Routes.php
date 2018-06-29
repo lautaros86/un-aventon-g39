9a1ec7e5d42c1496a03a3f2f6335b957270023be
Lautaro Silva Calistro <lsilva@fepba.gov.ar>
2018-06-29 13:44:54 -0300

    fix name file

--- application/Routes.php
+++ application/Routes.php
@@ -2,7 +2,7 @@
 
 $routes = array();
 $routes['registro'] = 'usuario/registro';
-$routes['perfil'] = 'usuario/verUsuario';
+$routes['perfil'] = 'usuario/verusuario';
 $routes['cerrarsesion'] = '/login/cerrar';
 $routes['preguntas-frecuentes'] = '/paginaestatica/preguntasFrecuentes';
 $routes['pruebas/(:any)/(:any)'] = 'usuario/test/$1/$2';
